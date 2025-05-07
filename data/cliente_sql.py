460446dd91c66a440695fd2779fa7f8f6e83cb72
Maria Eduarda Pinheiro Mozer <mariaeduardamozer.ifes@gmail.com>
2025-05-07 18:39:11 -0300

    versao final

---
+++ data/cliente_sql.py
@@ -0,0 +1,28 @@
+CRIAR_TABELA = """
+CREATE TABLE IF NOT EXISTS cliente (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nome TEXT NOT NULL,
+cpf TEXT NOT NULL,
+email TEZT NOT NULL,
+telefone TEXT NOT NULL,
+senha TEXT NOT NULL)
+"""
+
+INSERIR_CLIENTE = """
+INSERT INTO cliente (nome, cpf, email, telefone, senha)
+VALUES (?, ?, ?, ?, ?)
+"""
+
+OBTER_TODOS = """
+SELECT
+    id,
+    nome,
+    cpf,
+    email,
+    telefone,
+    senha
+FROM cliente
+"""
+
+
+
--- main.py
+++ main.py
@@ -4,19 +4,26 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 
 from data import produto_repo
+from data import cliente_repo
 
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 produto_repo.criar_tabela()
+cliente_repo.criar_tabela()
 
 
 @app.get("/")
 async def root():
     produtos = produto_repo.obter_todos()
     response = templates.TemplateResponse("index.html", {"request": {},"produtos": produtos})
-    
+    return response
+
+@app.get("/clientes")
+async def root():
+    cliente = cliente_repo.obter_todos()
+    response = templates.TemplateResponse("clientes.html", {"request": {},"cliente": cliente})
     return response
 
 
